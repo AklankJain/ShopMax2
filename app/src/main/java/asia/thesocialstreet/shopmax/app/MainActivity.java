f19da3288cd9e6eaf0e4f587884f8765cc745382
AklankJain <aj.aklank@live.com>
2016-06-17 11:30:44 +0530

    First Commit

---
+++ app/src/main/java/asia/thesocialstreet/shopmax/app/MainActivity.java
@@ -0,0 +1,220 @@
+package asia.thesocialstreet.shopmax.app;
+
+import android.*;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.location.Geocoder;
+import android.location.Location;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.identity.intents.Address;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private GoogleApiClient mGoogleApiClient;
+    ListView list;
+    private Location mLastLocation;
+    double mLatitudeText;
+    double mLongitudeText;
+    //Geocoder geoCoder = new Geocoder(this, Locale.getDefault());
+    //StringBuilder builder = new StringBuilder();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = (ListView) findViewById(R.id.listView);
+        list.setAdapter(new AklankAdapter(this));
+        if (mGoogleApiClient == null) {
+            // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
+            // See https://g.co/AppIndexing/AndroidStudio for more information.
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .addApi(AppIndex.API).build();
+        }
+
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://asia.thesocialstreet.shopmax.app/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(mGoogleApiClient, viewAction);
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://asia.thesocialstreet.shopmax.app/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(mGoogleApiClient, viewAction);
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        List<android.location.Address> address;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitudeText = mLastLocation.getLatitude();
+            mLongitudeText = mLastLocation.getLongitude();
+            Log.d("Latitude", "Value: " + mLatitudeText);
+            Log.d("Logitude" , "Value: " + mLongitudeText);
+            Geocoder geoCoder = new Geocoder(this, Locale.getDefault());
+            StringBuilder builder = new StringBuilder();
+            try {
+                address = geoCoder.getFromLocation(mLatitudeText,mLongitudeText, 1);
+                int maxLines = address.get(0).getMaxAddressLineIndex();
+                String city = address.get(0).getLocality();
+                Log.d("City", "Value:" + city);
+              //  setContentView(R.layout.activity_main);
+                TextView textView = (TextView) findViewById(R.id.place);
+                textView.setText(city);
+                for (int i=0; i<maxLines; i++) {
+                    String addressStr = address.get(0).getAddressLine(i);
+                    builder.append(addressStr);
+                    builder.append(" ");
+                }
+
+            } catch (IOException e) {
+                // Handle IOException
+            } catch (NullPointerException e) {
+                // Handle NullPointerException
+            }
+            String finalAddress = builder.toString(); //This is the complete address.
+            Log.d("Address", "Value:" + finalAddress);
+
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
+
+class SingleRow{
+    String title;
+    String desc;
+    int image;
+    SingleRow(String title , String desc , int image)
+    {
+        this.title = title;
+        this.desc = desc;
+        this.image = image;
+    }
+}
+class AklankAdapter extends BaseAdapter {
+
+    ArrayList<SingleRow> list;
+    Context context;
+    AklankAdapter(Context c)
+    {
+        context = c;
+        list = new ArrayList<SingleRow>();
+        Resources res = c.getResources();
+        String[] titles = res.getStringArray(R.array.titles);
+        String[] desc = res.getStringArray(R.array.desc);
+        int[] images = {R.drawable.image1 , R.drawable.image1 , R.drawable.image1 , R.drawable.image1 , R.drawable.image1 , R.drawable.image1 , R.drawable.image1 };
+        for (int i =0;i<7;i++)
+        {
+            list.add(new SingleRow(titles[i] , desc[i] ,images[i]));
+        }
+    }
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return list.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.single_row ,parent, false);
+        TextView title = (TextView) row.findViewById(R.id.title);
+        TextView desc = (TextView) row.findViewById(R.id.desc);
+        ImageView image = (ImageView) row.findViewById(R.id.imageView);
+
+        SingleRow temp = list.get(i);
+
+        title.setText(temp.title);
+        desc.setText(temp.desc);
+        image.setImageResource(temp.image);
+
+
+        return row;
+    }
+}
